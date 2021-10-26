30dc0e1ce8784fb5472feaac24637795436c1d75
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2021-10-26 12:18:52 +0300

    Update Cipher Class

--- src/main/java/CaeserCipher.java
+++ src/main/java/CaeserCipher.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class CaeserCipher {
-    public static void main(String arg[]){
+    public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Message");
         String plaintext = sc.nextLine();
@@ -9,22 +9,25 @@ public class CaeserCipher {
         int shift = sc.nextInt();
         String ciphertext = "";
         char alphabet;
-        for (int i=0;i<plaintext.length(); i++){
+        for (int i = 0; i < plaintext.length(); i++) {
             alphabet = plaintext.charAt(i);
-            if(alphabet >= 'a' && alphabet >= 'z'){
-                alphabet = (char)(alphabet+shift);
-                if(alphabet > 'z'){
-                    alphabet = (char) (alphabet+'a' - 'z' - 1);
-
+            if (alphabet >= 'a' && alphabet >= 'z') {
+                alphabet = (char) (alphabet + shift);
+                if (alphabet > 'z') {
+                    alphabet = (char) (alphabet + 'a' - 'z' - 1);
                 }
-                ciphertext = ciphertext+ alphabet;
+                ciphertext = ciphertext + alphabet;
             }
-            else if(alphabet >= 'A' && alphabet <== 'Z'){
-                alphabet = (char) (alphabet+shift);
-                if(alphabet > 'Z')
-                    alphabet=(char) (alphabet+ 'A' - 'Z' -1);
+            else if (alphabet >= 'A' && alphabet <= = 'Z') {
+                alphabet = (char) (alphabet + shift);
+                if (alphabet > 'Z')
+                    alphabet = (char) (alphabet + 'A' - 'Z' - 1);
             }
+            ciphertext = ciphertext + alphabet;
 
+        else{
+            ciphertext = ciphertext + alphabet;
         }
-    }
+        System.out.println("Cipher text" + ciphertext);
+    }}
 }

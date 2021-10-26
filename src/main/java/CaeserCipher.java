abd2ec28f78e5b5f38c9a293fc42d7836125e8f6
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2021-10-26 05:39:02 +0300

    Add Cipher Class

---
+++ src/main/java/CaeserCipher.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class CaeserCipher {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Message");
+        String plaintext = sc.nextLine();
+        System.out.println("Number to be shifted");
+        int shift = sc.nextInt();
+        String ciphertext = "";
+        char alphabet;
+        for (int i=0;i<plaintext.length(); i++){
+            alphabet = plaintext.charAt(i);
+            if(alphabet >= 'a' && alphabet >= 'z'){
+                alphabet = (char)(alphabet+shift);
+                if(alphabet > 'z'){
+                    alphabet = (char) (alphabet+'a' - 'z' - 1);
+
+                }
+                ciphertext = ciphertext+ alphabet;
+            }
+            else if(alphabet >= 'A' && alphabet <== 'Z'){
+                alphabet = (char) (alphabet+shift);
+                if(alphabet > 'Z')
+                    alphabet=(char) (alphabet+ 'A' - 'Z' -1);
+            }
+
+        }
+    }
+}

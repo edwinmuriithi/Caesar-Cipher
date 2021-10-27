c8a7af3e0684f39843dc04ab6d38b2f8c1e158ac
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2021-10-27 05:24:06 +0300

    Modify decrypt method

--- src/main/java/cipher/CaeserCipher.java
+++ src/main/java/cipher/CaeserCipher.java
@@ -4,14 +4,39 @@ import java.util.Scanner;
 
 public class CaeserCipher {
 
-    public static String encrypt(){
+    static String alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static String encrypt(String pText, int Key){
+        pText = pText.toLowerCase();
+        String cText = "";
+        for(int i = 0; i<pText.length(); i++){
+            int charIndex = alphabet.indexOf(pText.charAt(i));
+            int newIndex = (charIndex + Key) % 26;
+            char cipherChar = alphabet.charAt(newIndex);
+            cText = cText + cipherChar;
+        }
+
+        return cText;
 
     }
 
-    public static String decrypt(){
+    public static String decrypt(String cText, int Key){
+        cText = cText.toLowerCase();
+        String pText = "";
+        for (int i = 0; i<cText.length(); i++){
+            int charIndex = alphabet.indexOf(cText.charAt(i));
+            int newIndex = (charIndex - Key) % 26;
+            if (newIndex < 0){
+                newIndex = alphabet.length() + newIndex;
+            }
+            char plainChar = alphabet.charAt(newIndex);
+            pText = pText + plainChar;
+        }
 
 
+        return pText;
     }
+
     public static void main(String arg[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Message");
